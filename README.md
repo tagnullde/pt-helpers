fab6635f5abc5598037e31ec59ff28c15b8eb9a9
tagnullde <git@tagnull.de>
2023-11-13 15:50:58 +0100

    added domain_info.sh, updated README and fixed some bugs.

--- README.md
+++ README.md
@@ -1,26 +1,61 @@
-# What?
+# Whats this?
 
-This script helps me during the early stages of a pentest.
+These scripts helps me during the early stages of a pentest.
+
+1. domain_info.sh
+2. initial_scan.sh
 
 # How?
 
+The scripts should be run one after another. Depending on the situation.
+
+## domain_info.sh
+
+```
+./domain_info.sh -u <user> -p <password> -h <dc-ip>
+```
+
+I assume the following setup:
+
+1. We have: AD-User credentials
+2. We have: dc-ip
+3. nxc    : is installed
+4. hosts file: contains DC and Domain of target domain 
+
+The script does:
+
+- Get Subnets (File will be used by **initial_scan.sh**)
+- Grab Password-Policy
+- Create Bloodhound.zip
+- Grab User-Descriptions
+- Find common vulns: PetitPotam, ZeroLogon, NoPac
+- List all shares the user has access to
+- Get gpp_autologin (soon)
+- Get gpp_password (soon)
+
+## initial_scan.sh
+
+```
+./initial_scan.sh <subnet-file>
+```
+
+I assume the following setup:
+
+1. nmap is installed
+2. eyewitness is installed
+
 At the moment it does the following:
 
-1. Do a fast nmap scan over all provided ("in-scope") subnets.
-2. Get all hosts that are up and do a full scan (only top 2000 ports).
-3. Use the nmap XML output to feed "eyewitness" to screenshot most HTTP Services to get a lay of the land.
+- Do a fast nmap scan over all provided ("in-scope") subnets.
+- Get all hosts that are up and do a full scan (only top 2000 ports).
+- Use the nmap XML output to feed "eyewitness" to screenshot most HTTP Services to get a lay of the land.
 
 # That's all?
 
-For now it is. I will add more features over time.
+For now that's it. Any wishes?
 
-# Like what?
+# ToDo
 
-I will put the most common tasks I do into this script if I can. Like bulk nxc checks:
-
-- PetitPotam
-- ZeroLogon
-- Password Policy
-- GPP-Passwords
-- User Desc
-- etc.
+- Right now all files are just dumped into the same folder. I need to add some organization.
+- `gpp_autologin` and `gpp_password` are not yet implemented.
+- Probably killing some bugs...
---
+++ domain_info.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+while getopts ":u:p:h:" opt; do
+  case $opt in
+    u)
+      user="$OPTARG"
+      ;;
+    p)
+      passwd="$OPTARG"
+      ;;
+    h)
+      dc="$OPTARG"
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+    :)
+      echo "Option -$OPTARG requires an argument." >&2
+      exit 1
+      ;;
+  esac
+done
+
+# Display help if arguments are missing
+if [ -z "$user" ] || [ -z "$passwd" ] || [ -z "$dc" ]; then
+  echo "Usage: $0 -u <user> -p <password> -h <dc-ip>"
+  exit 1
+fi
+
+
+# get all subnets from sites and services
+echo "[!] Creating Subnets.txt"
+
+nets=$(nxc ldap $dc -u $user -p $passwd -M subnets | grep Subnet: | cut -d '(' -f 2 | cut -d ')' -f 1 | cut -d ':' -f 2) 
+
+for net in "${nets[@]}"; do
+  echo "$net"
+done > subnets.txt
+
+echo -e "[+] Done: subnets.txt \n"
+
+
+# get password policy
+echo "[!] Grabbing Password Policy"
+nxc smb $dc -u $user -p $passwd --pass-pol | grep -v -e "[*]" -e "[+]" -e "[-]" | cut -d " " -f 28- > password-policy.txt
+echo -e "[+] Done: password-policy.txt \n"
+
+# get bloodhound data (set domain and dc in hostfile first)
+echo "[!] Gathering Bloodhound Data"
+bh=$(nxc ldap $dc -u $user -p $passwd --bloodhound -ns $dc --collection All | grep "Compressing" | cut -d " " -f 30)
+echo -e "[+] Done: $bh \n"
+
+# get vulns
+echo "[!] Searching for AD-Vulnerabilities"
+nxc ldap $dc -u $user -p $passwd -M get-desc-users | grep "User: " | cut -d " " -f 20- > user-descriptions.txt
+
+nxc smb $dc -u $user -p $passwd -M petitpotam | grep "VULNERABLE" > ad-vulns.txt
+nxc smb $dc -u $user -p $passwd -M zerologon | grep "VULNERABLE" >> ad-vulns.txt
+nxc smb $dc -u $user -p $passwd -M nopac | grep "VULNERABLE" >> ad-vulns.txt
+echo "[+] Done: user-descriptions.txt"
+echo -e "[+] Done: ad-vulns.txt \n"
+
+# get shares
+echo "[!] Getting Shares."
+for net in "${nets[@]}"; do
+   nxc --no-progress smb $net -u $user -p $passwd --shares | grep -v -e "[*]" -e "[+]" -e "[-]" >> shares.txt
+   echo -e "------------------ \n" >> shares.txt
+done
+echo -e "[+] Done: shares.txt \n"
+
+# get gpp_autologin and gpp_password (Not done yet.)
+#nxc smb $dc -u $user -p $passwd -M gpp_autologin | grep -v -e "[*]" -e "[+]" -e "[-]"
+#nxc smb $dc -u $user -p $passwd -M gpp_password | grep -v -e "[*]" -e "[+]" -e "[-]"
